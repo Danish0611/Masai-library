909197f2a595b7a83a82f6d987f638ddfc514c04
Mohd Danish <113216768+Danish0611@users.noreply.github.com>
2023-02-09 13:11:27 +0530

    Update index.js

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,1 +1,16 @@
 // you can write your js code here
+let bookData=JSON.parse(localStorage.getItem("book-list"))||[];
+document.getElementById("home").addEventListener("submit",function (event){
+    event.preventDefault();
+    const bookName=document.getElementById("name").value;
+    const Author=document.getElementById("author").value;
+    const Description=document.getElementById("desc").value;
+    const AddedDate=document.getElementById("added").value;
+    const Category=document.getElementById("category").value;
+    const price=document.getElementById("price").value;
+    let data={bookName,Author,Description,AddedDate,Category,price};
+
+    bookData.push(data);
+    localStorage.setItem("book-list",JSON.stringify(bookData));
+    
+})
